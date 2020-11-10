e37d3f79253df5d6af4700311fdfd319bfdcdb69
souvik-pl <70844358+souvik-pl@users.noreply.github.com>
2020-11-11 01:27:32 +0530

    try-catch included in the code

--- app.js
+++ app.js
@@ -18,50 +18,56 @@ app.get('/',(req,res) => {
 
 
 app.post('/register', async (req, res) => {
-
-  let foundUser = users.find((data) => req.body.email === data.email);
-  if (!foundUser) {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (!foundUser) {
     
-    let hashPassword = await bcrypt.hash(req.body.password, 10);
-    //add to the users data
-    let newUser = {
-      id: Date.now(),
-      username: req.body.username,
-      email: req.body.email,
-      password: hashPassword,
-    };
-    users.push(newUser);
-    console.log('User list', users);
+            let hashPassword = await bcrypt.hash(req.body.password, 10);
     
-    res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
-  } else {
-    res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
-  }
+            let newUser = {
+                id: Date.now(),
+                username: req.body.username,
+                email: req.body.email,
+                password: hashPassword,
+            };
+            users.push(newUser);
+            console.log('User list', users);
+    
+            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
+        } else {
+            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
 });
 
 app.post('/login', async (req, res) => {
-
-  let foundUser = users.find((data) => req.body.email === data.email);
-  if (foundUser) {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (foundUser) {
     
-    let submittedPass = req.body.password; 
-    let storedPass = foundUser.password; 
+            let submittedPass = req.body.password; 
+            let storedPass = foundUser.password; 
     
-    const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
-    if (passwordMatch) {
-      let usrname = foundUser.username;
-      res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
-    } else {
-      res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
-      }
+            const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+            if (passwordMatch) {
+                let usrname = foundUser.username;
+                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+            } else {
+            res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+            }
+        }
+        else {
+    
+            let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+            await bcrypt.compare(req.body.password, fakePass);
+    
+            res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
+        }
+    } catch{
+        res.send("Internal server error");
     }
-    else {
-    
-    let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
-    await bcrypt.compare(req.body.password, fakePass);
-    
-    res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
-  }
 });
 
 
