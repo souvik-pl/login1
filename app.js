f2e27b02c18fff42854f2082c3ed4ac18f0555de
souvik-pl <70844358+souvik-pl@users.noreply.github.com>
2020-11-11 00:21:16 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const http = require('http');
+const bcrypt = require('bcrypt');
+const path = require("path");
+const bodyParser = require('body-parser');
+const users = require('./data').userDB;
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'./public')));
+
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+});
+
+
+app.post('/register', async (req, res) => {
+
+  let foundUser = users.find((data) => req.body.email === data.email);
+  if (!foundUser) {
+    
+    let hashPassword = await bcrypt.hash(req.body.password, 10);
+    //add to the users data
+    let newUser = {
+      id: Date.now(),
+      username: req.body.username,
+      email: req.body.email,
+      password: hashPassword,
+    };
+    users.push(newUser);
+    console.log('User list', users);
+    
+    res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
+  } else {
+    res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+  }
+});
+
+app.post('/login', async (req, res) => {
+
+  let foundUser = users.find((data) => req.body.email === data.email);
+  if (foundUser) {
+    
+    let submittedPass = req.body.password; 
+    let storedPass = foundUser.password; 
+    
+    const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+    if (passwordMatch) {
+      let usrname = foundUser.username;
+      res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+    } else {
+      res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+      }
+    }
+    else {
+    
+    let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+    await bcrypt.compare(req.body.password, fakePass);
+    
+    res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
+  }
+});
+
+
+server.listen(3000, function(){
+    console.log("server is listening on port: 3000");
+});
